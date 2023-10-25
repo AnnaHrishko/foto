043eba60e1cb6a4f87517c5191457511902d072a
unknown <annahrishko@ukr.net>
2023-10-25 12:51:54 +0300

    ediy country select, add arrow in swiper

--- front/js/script.js
+++ front/js/script.js
@@ -29,6 +29,44 @@ $('.sec-instruction__inner__instruction-navigation ul li').click(function(){
 //     }
 // });
 
+// Template function which adds CSS flag and displays country name
+function flagTemplate(country){
+    return $("<span class='flag-icon flag-icon-" + country.id + " '></span><span class='flag-text'>" + country.text + "</span>");
+  }
+  
+  // Generate correct URL based on entered search term
+  function generateUrl(params){
+    if(params.term){
+      return "https://restcountries.com/v3.1/name/" + params.term;
+    } else {
+      return "https://restcountries.com/v3.1/all";
+    }
+  }
+  
+  // Initialise select2 using flagTemplate function for both result and selection
+  $('.country-select').select2({
+    // Set template for results and selection
+    templateResult: flagTemplate,
+    templateSelection: flagTemplate,
+    // Set placeholder text
+    placeholder: 'Сountry',
+    // Load country list from https://restcountries.com
+    ajax: {
+      url: generateUrl,
+      cache: 250,
+      dataType: "json",
+      processResults: function(data) {      
+        return {
+          results: data
+            .map(x => ({id: x.cca2.toLowerCase(), text: x.name.common}))
+            .sort((a, b) => ('' + a.text).localeCompare(b.text))
+        };
+      }
+    }
+  });
+  
+  
+
 $('.faq-wrapper .content').hide();
 $('.faq-wrapper .accordion-sec').click(function() {
   $(this).toggleClass('active')
